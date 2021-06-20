1828cecc9ce882dcbbc28bec41a53027b1db9b7a
themx05 <maximiliencomlan05@gmail.com>
2021-06-20 19:50:06 +0100

    Added reporting url, reporting script

--- src/components/TrackingPoint.tsx
+++ src/components/TrackingPoint.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Button, Icon, Message } from "semantic-ui-react";
+import { Button, Icon, Message, Container } from "semantic-ui-react";
+import { isDev } from "../core/utils";
 import styles from "../styles/TrackingPoint.module.scss";
 
 interface Props {
@@ -7,22 +8,39 @@ interface Props {
 }
 
 function TrackingPoint(props: Props) {
+    const [copied, setCopied] = React.useState(false);
+
+    let url = `${isDev() ? "http://localhost:4000" : "https://api.traffichub.co"}/report/${props.tracker.tag}.js`
+    let script = `<script src="${url}" defer></script>`;
+
+    function onCopy() {
+        navigator
+            .clipboard
+            .writeText(script)
+            .then((done) => {
+                setCopied(true);
+                setTimeout(() => {
+                    setCopied(false);
+                }, 3000);
+            })
+    }
+
     return <div>
         <h2>{props.tracker.title}</h2>
-        <div>
-            <Message>
-                <Message.Header>Ajoutez ce code aux pages du site {props.tracker.url}</Message.Header>
-                <Message.Content>
-                    <code>
-                        {`<script src="https://api.traffichub.co/report/${props.tracker.tag}" defer></script>`}
-                    </code>
-                    <Button color="blue">
-                        <Icon name={"copy"} />
-                        Copier
+        <Message style={{ background: '#434343', color: 'wheat' }}>
+            <Message.Header>Ajoutez ce code aux pages du site {props.tracker.url}</Message.Header>
+            <Message.Content style={{ flexDirection: 'column' }}>
+                <code style={{ background: '#636363', display: 'block', padding: '8px 16px', borderRadius: '4px', margin: '16px 0px' }}>
+                    {script}
+                </code>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end' }}>
+                    <Button color={copied ? "green" : "blue"} onClick={onCopy} disabled={copied}>
+                        <Icon name={copied ? "check" : "copy"} />
+                        {copied ? "Copié" : "Copier"}
                     </Button>
-                </Message.Content>
-            </Message>
-        </div>
+                </div>
+            </Message.Content>
+        </Message>
     </div>
 }
 
